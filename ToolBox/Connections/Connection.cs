d2474818fe58f50daa0f2c35888d6e7a49ff78e1
Andrushka18 <andre_kalumba@yahoo.fr>
2020-04-03 16:39:02 +0200

    initiation projet

---
+++ ToolBox/Connections/Connection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace ToolBox.Connections
+{
+    public class Connection
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:44356/api/");
+            HttpResponseMessage response = client.GetAsync("").Result;
+            Console.WriteLine("{0} - {1}", (int)response.StatusCode, response.ReasonPhrase);
+            if ((int)response.StatusCode == 401)
+            {
+                var credentials = new NetworkCredential("Admin", "Admin");
+                var handler = new HttpClientHandler { Credentials = credentials };
+
+                client = new HttpClient(handler);
+                client.BaseAddress = new Uri("http://localhost:5629/api/");
+                response = client.GetAsync(" ").Result;
+                Console.WriteLine("{0} - {1}", (int)response.StatusCode, response.ReasonPhrase);
+            }
+            if (response.IsSuccessStatusCode)
+            {
+                IEnumerable<T> Get()  user = (IEnumerable<T>)response.Content.ReadAsAsync<IEnumerable<T>>().Result;
+
+                foreach (var (T entity)  in IEnumerable<T>)
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}"
+                   
+                };
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
